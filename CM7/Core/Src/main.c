928b436c20e88aed930f75dbfc6a0a0c7edfd6bc
Vignesh Selvaraj <vselvaraj@wisc.edu>
2026-03-30 01:01:30 -0500

    reduced mem_size for lwip

--- CM7/Core/Src/main.c
+++ CM7/Core/Src/main.c
@@ -234,7 +234,7 @@ Error_Handler();
 
   /* Create the thread(s) */
   /* definition and creation of defaultTask */
-  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 256);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
