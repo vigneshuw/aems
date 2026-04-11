f9fba4f8bfdb87b53f07f5968166d0717d6e53fb
Vignesh Selvaraj <vselvaraj@wisc.edu>
2026-04-11 12:58:30 -0500

    removed the Debug tracking, same for release

--- CM7/Debug/Drivers/BSP/Components/lan8742.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/lan8742.o: \
- C:/Users/vigne/STM32CubeIDE/workspace/AEMSv02-Firmware/Drivers/BSP/Components/lan8742/lan8742.c \
- C:/Users/vigne/STM32CubeIDE/workspace/AEMSv02-Firmware/Drivers/BSP/Components/lan8742/lan8742.h
-C:/Users/vigne/STM32CubeIDE/workspace/AEMSv02-Firmware/Drivers/BSP/Components/lan8742/lan8742.h:
